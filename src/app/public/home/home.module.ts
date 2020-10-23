d7bfc3815d3363cc3afd0c3a2105d445fb948d74
Guillem CANTO <guillem.canto@telecom-sudparis.eu>
2020-10-23 17:43:04 +0200

    Fix error in prod

--- src/app/public/home/home.module.ts
+++ src/app/public/home/home.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { HomeBannerComponent } from './home-banner/home-banner.component';
 import { HomeFeatureCardComponent } from './home-feature-card/home-feature-card.component';
 import { HomeFeaturesComponent } from './home-features/home-features.component';
+import {PublicRoutingModule} from '../public-routing.module';
 
 
 
 @NgModule({
   declarations: [HomeComponent, HomeBannerComponent, HomeFeatureCardComponent, HomeFeaturesComponent],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        PublicRoutingModule
+    ]
 })
 export class HomeModule { }
